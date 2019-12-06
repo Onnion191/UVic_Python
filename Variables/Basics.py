d3deea92d9249a60ef277bfc2dc4952c2b73cc18
Aleix Rius <u1940025@campus.udg.edu>
2019-12-06 12:36:58 +0100

    first Commit

---
+++ Variables/Basics.py
@@ -0,0 +1,31 @@
+#### Tipus de variables ###
+
+#Numeriques
+numero = 10
+print ("El numero es un enter de valor ", numero)
+
+numero = 10.01
+print ("El numero ara es decimal: ", numero)
+
+#Caracters
+caracter = 'a'
+print ("El meu caracter es la lletra: " , caracter)
+
+caracter = '<' #Pot ser qualsevol simbol
+print ("El caracter ara es un simbol ",caracter)
+
+ConjuntCaracters = "Hola soc molts caracters junts" #Es poden fer un conjunt de caracters per fer una paraula, una frase, etc.
+print ("El conjunt de caracters: ",ConjuntCaracters)
+
+#AL LLORO!  si nomes volem 1 caracter, hem d'utiltizar '' en canvi si volem mes de un, hem d'utilitzar ""
+
+Un = 'a'
+MesDun = "aaaa aaa aa  a aa aa "
+
+
+#Logics o booleans
+
+Logic = True;
+print ("Logic es ",Logic);
+Logic = False
+print ("Logic es ",Logic);
